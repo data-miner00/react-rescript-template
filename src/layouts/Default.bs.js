b2d07c54a37f5bee9b84ce255469ffd82f20afc9
data-miner00 <data-miner00@users.noreply.github.com>
2022-12-17 14:49:29 +0800

    style: Added container layout

--- src/layouts/Default.bs.js
+++ src/layouts/Default.bs.js
@@ -7,7 +7,9 @@ function Default(Props) {
   var children = Props.children;
   return React.createElement("div", {
               className: "bg-gradient-to-br from-cyan-600 to-violet-400 min-h-screen"
-            }, React.createElement(Header$RescriptProjectTemplate.make, {}), children);
+            }, React.createElement(Header$RescriptProjectTemplate.make, {}), React.createElement("main", {
+                  className: "max-w-[1400px] mx-auto"
+                }, children));
 }
 
 var make = Default;
--- src/layouts/Default.res
+++ src/layouts/Default.res
@@ -1,8 +1,9 @@
-
 @react.component
 let make = (~children: React.element) => {
-    <div className="bg-gradient-to-br from-cyan-600 to-violet-400 min-h-screen">
-        <Header />
-        children
-    </div>
+  <div className="bg-gradient-to-br from-cyan-600 to-violet-400 min-h-screen">
+    <Header />
+    <main className="max-w-[1400px] mx-auto">
+      children
+    </main>
+  </div>
 }
