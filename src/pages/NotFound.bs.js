3a1b9af7f43315be191047d867ddfbd5796651cc
data-miner00 <data-miner00@users.noreply.github.com>
2023-04-14 19:12:21 +0800

    fix: Dark mode style for 404

--- src/pages/NotFound.bs.js
+++ src/pages/NotFound.bs.js
@@ -7,12 +7,12 @@ function NotFound(Props) {
   return React.createElement("div", {
               className: "py-[150px] pt-28 flex flex-col items-center"
             }, React.createElement("h1", {
-                  className: "font-bold text-5xl tracking-wide uppercase block mb-2 text-slate-800"
+                  className: "font-bold text-5xl tracking-wide uppercase block mb-2 text-slate-800 dark:text-white"
                 }, "404 Not Found"), React.createElement("p", {
-                  className: "font-semibold text-xl block text-gray-600 mb-4"
+                  className: "font-semibold text-xl block text-gray-600 mb-4 dark:text-gray-300"
                 }, "The page that you are trying to access could not be found."), React.createElement(Link$RescriptProjectTemplate.make, {
                   to: "..",
-                  className: "py-1 px-4 block border border-solid border-gray-200 rounded text-xl hover:bg-gray-50",
+                  className: "py-1 px-4 block border border-solid border-gray-200 rounded text-xl hover:bg-gray-50 dark:hover:bg-gray-600 dark:border-gray-600",
                   children: "Return"
                 }));
 }
