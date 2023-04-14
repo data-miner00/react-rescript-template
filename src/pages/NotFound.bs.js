7c11e58b9627c619fe1fba045678aa64ecc32b6b
data-miner00 <data-miner00@users.noreply.github.com>
2023-04-14 20:52:39 +0800

    fix: Responsive style for 404

--- src/pages/NotFound.bs.js
+++ src/pages/NotFound.bs.js
@@ -5,14 +5,14 @@ import * as Link$RescriptProjectTemplate from "../components/Link.bs.js";
 
 function NotFound(Props) {
   return React.createElement("div", {
-              className: "py-[150px] pt-28 flex flex-col items-center"
+              className: "py-[150px] pt-28 flex flex-col px-10 md:px-0 items-center  text-center"
             }, React.createElement("h1", {
-                  className: "font-bold text-5xl tracking-wide uppercase block mb-2 text-slate-800 dark:text-white"
+                  className: "font-bold text-4xl md:text-5xl tracking-wide uppercase block mb-2 text-slate-800 dark:text-white"
                 }, "404 Not Found"), React.createElement("p", {
-                  className: "font-semibold text-xl block text-gray-600 mb-4 dark:text-gray-300"
+                  className: "font-semibold text-lg md:text-xl block text-gray-600 mb-4 dark:text-gray-300"
                 }, "The page that you are trying to access could not be found."), React.createElement(Link$RescriptProjectTemplate.make, {
                   to: "..",
-                  className: "py-1 px-4 block border border-solid border-gray-200 rounded text-xl hover:bg-gray-50 dark:hover:bg-gray-600 dark:border-gray-600",
+                  className: "py-1 text-lg px-4 block border border-solid border-gray-200 rounded md:text-xl hover:bg-gray-50 dark:hover:bg-gray-600 dark:border-gray-600",
                   children: "Return"
                 }));
 }
--- src/pages/NotFound.res
+++ src/pages/NotFound.res
@@ -1,8 +1,8 @@
 @react.component
 let make = () => {
-  <div className="py-[150px] pt-28 flex flex-col items-center">
-    <h1 className="font-bold text-5xl tracking-wide uppercase block mb-2 text-slate-800 dark:text-white">{"404 Not Found"->React.string}</h1>
-    <p className="font-semibold text-xl block text-gray-600 mb-4 dark:text-gray-300">{"The page that you are trying to access could not be found."->React.string}</p>
-    <Link className="py-1 px-4 block border border-solid border-gray-200 rounded text-xl hover:bg-gray-50 dark:hover:bg-gray-600 dark:border-gray-600" to="..">{"Return"->React.string}</Link>
+  <div className="py-[150px] pt-28 flex flex-col px-10 md:px-0 items-center  text-center">
+    <h1 className="font-bold text-4xl md:text-5xl tracking-wide uppercase block mb-2 text-slate-800 dark:text-white">{"404 Not Found"->React.string}</h1>
+    <p className="font-semibold text-lg md:text-xl block text-gray-600 mb-4 dark:text-gray-300">{"The page that you are trying to access could not be found."->React.string}</p>
+    <Link className="py-1 text-lg px-4 block border border-solid border-gray-200 rounded md:text-xl hover:bg-gray-50 dark:hover:bg-gray-600 dark:border-gray-600" to="..">{"Return"->React.string}</Link>
   </div>
 }
